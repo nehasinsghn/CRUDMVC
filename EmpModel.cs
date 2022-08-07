8aa74c9977025387b7f536890e025952966ac78b
nehasinsghn <97651021+nehasinsghn@users.noreply.github.com>
2022-08-08 00:47:13 +0530

    Add files via upload

---
+++ EmpModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataFirstApproach.Models
+{
+    public class EmpModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public Nullable<int> Salary { get; set; }
+
+    }  
+}
